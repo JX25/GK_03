b58af9d242d4f6f55831eead0cba141b9c4a3d8c
Jakub Wozniak <jeehox25@gmail.com>
2019-04-11 22:44:30 +0200

    GK03

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GK_03
+{
+    public partial class Form1 : Form
+    {
+        private System.Drawing.Graphics g;
+        private System.Drawing.Pen pen1 = new System.Drawing.Pen(Color.Red, 1);
+        private System.Drawing.Pen pen2 = new System.Drawing.Pen(Color.Teal, 1);
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
